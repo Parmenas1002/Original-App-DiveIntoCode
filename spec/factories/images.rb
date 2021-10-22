e598edb53bf8efc3358a1d577b26c8161de433a3
Nicanor <zinkoparmenas@yahoo.com>
2021-10-22 14:05:14 +0000

    Add corrections

--- spec/factories/images.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-    factory :image do
-      images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/image1.jpg'), 'spec/fixtures/image1.jpg') ] }
-      post
-    end
-  end
